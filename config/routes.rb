9fc5df278e26d7e6a59a633f90eb3f06f85e9883
Sze Rong Tham <szerongtham@gmail.com>
2023-08-05 14:02:02 +0800

    primary skeleton is created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # READ
+  get "tasks", to: "tasks#index"
+
+  # CREATE
+  get "tasks/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit"
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to: "tasks#destroy"
 end
